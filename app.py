d52e9992e853491b932d193585e833995d4e821b
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-12 19:56:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ if __name__ == '__main__':
     print("🚀 Iniciando NH Transportes...")
     init_db()
     print("🌐 Sistema online!")
+    import os
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
