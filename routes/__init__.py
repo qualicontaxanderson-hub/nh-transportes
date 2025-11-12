bb3db9165580213d11bd6d8ef2e1e22892cb9563
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-12 20:17:46 -0300

    Create __init__.py

---
+++ routes/__init__.py
@@ -0,0 +1,8 @@
+**Caminho no GitHub:** `routes/__init__.py`
+
+```python
+from routes import clientes, fornecedores, fretes, motoristas, veiculos, relatorios
+__all__ = ['clientes', 'fornecedores', 'fretes', 'motoristas', 'veiculos', 'relatorios']
+```
+
+---
