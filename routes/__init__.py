c43af6b46733649d21e1793fd30d974592f8c413
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-15 11:31:05 -0300

    Register all blueprints including debug route

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,2 +1,9 @@
-
+# Importar os blueprints das rotas
+from .clientes import bp as clientes_bp
+from .motoristas import bp as motoristas_bp
+from .veiculos import bp as veiculos_bp
+from .fornecedores import bp as fornecedores_bp
+from .fretes import bp as fretes_bp
+from .relatorios import bp as relatorios_bp
+from .debug import bp as debug_bp
 
