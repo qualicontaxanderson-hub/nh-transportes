d2c47725101233ac489e25389decad32b1ca8af1
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-16 19:56:03 -0300

    Create motorista.py

---
+++ models/motorista.py
@@ -0,0 +1,25 @@
+from extensions import db
+from datetime import datetime
+
+
+class Motorista(db.Model):
+    __tablename__ = 'motoristas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(14), unique=True)
+    cnh = db.Column(db.String(20), unique=True)
+    categoria_cnh = db.Column(db.String(2))
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    endereco = db.Column(db.String(200))
+    cidade = db.Column(db.String(100))
+    estado = db.Column(db.String(2))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    # Relacionamento com fretes
+    fretes = db.relationship('Frete', backref='motorista', lazy=True, foreign_keys='Frete.motoristas_id')
+    
+    def __repr__(self):
+        return f'<Motorista {self.nome}>'
