10efa63c7bbc4b39e6c046515b1a9fd9e7e6e6fc
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-16 19:52:28 -0300

    Update quantidade.py

--- models/quantidade.py
+++ models/quantidade.py
@@ -1,1 +1,18 @@
+from extensions import db
+from datetime import datetime
+
+
+class Quantidade(db.Model):
+    __tablename__ = 'quantidades'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    valor = db.Column(db.Numeric(10, 2), nullable=False)
+    descricao = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Relacionamento com fretes
+    fretes = db.relationship('Frete', backref='quantidade', lazy=True)
+    
+    def __repr__(self):
+        return f'<Quantidade {self.valor}>'
 
