1583bf4196f505d3a03ae0e4d618b4adeadb62e1
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-16 19:54:09 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,22 @@
+from models.usuario import Usuario
+from models.cliente import Cliente
+from models.fornecedor import Fornecedor
+from models.motorista import Motorista
+from models.veiculo import Veiculo
+from models.quantidade import Quantidade
+from models.origem import Origem
+from models.destino import Destino
+from models.frete import Frete
+
+__all__ = [
+    'Usuario',
+    'Cliente',
+    'Fornecedor',
+    'Motorista',
+    'Veiculo',
+    'Quantidade',
+    'Origem',
+    'Destino',
+    'Frete'
+]
 
