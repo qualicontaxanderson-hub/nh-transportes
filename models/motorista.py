0b51c6ac398498f48d63ccf1ae5ad94fcafca203
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-16 20:00:17 -0300

    Update motorista.py

--- models/motorista.py
+++ models/motorista.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from extensions import db
 from datetime import datetime
 
@@ -17,9 +21,6 @@ class Motorista(db.Model):
     estado = db.Column(db.String(2))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
-    # Relacionamento com fretes
-    fretes = db.relationship('Frete', backref='motorista', lazy=True, foreign_keys='Frete.motoristas_id')
     
     def __repr__(self):
         return f'<Motorista {self.nome}>'
