10354bce9bcfa2d6dbc7bf32cd55670ebc5fdba5
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-18 20:34:44 -0300

    Create API endpoint for freight routes (CTe calculation)

---
+++ routes/api.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+from models.rota import Rota
+
+api_bp = Blueprint('api', __name__, url_prefix='/api')
+
+@api_bp.route('/rota/<int:origem_id>/<int:destino_id>', methods=['GET'])
+def get_rota(origem_id, destino_id):
+    """Buscar valor de CTe por rota (ORIGEM x DESTINO)"""
+    rota = Rota.query.filter_by(
+        origem_id=origem_id,
+        destino_id=destino_id,
+        ativo=True
+    ).first()
+    
+    if rota:
+        return jsonify({'valor_por_litro': rota.valor_por_litro})
+    else:
+        return jsonify({'valor_por_litro': 0}), 404
