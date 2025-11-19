c8fb6a65c0e7e6d7a22f280df83827127529f291
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-19 10:47:31 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 from models.usuario import Usuario
+from models.rota import Rota
+from models.motorista import Motorista
 
-__all__ = ['Usuario', 'db']
+__all__ = ['Usuario', 'Rota', 'Motorista', 'db']
