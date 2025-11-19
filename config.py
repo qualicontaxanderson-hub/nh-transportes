0a2e920082f4ad43818759260344b02f45bedef7
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-18 23:14:31 -0300

    Add SQLALCHEMY_DATABASE_URI to config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     DB_USER = "root"
     DB_PASSWORD = "CYTzzRYLVmEJGDexxXpgepWgpvebdSrV"
     DB_NAME = "railway"
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SECRET_KEY = os.environ.get('SECRET_KEY') or "nh-transportes-2025-secret"
     APP_NAME = "NH Transportes"
     DEBUG = False  # Mudei para False em produção
