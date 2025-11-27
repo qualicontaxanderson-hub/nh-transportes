238fe4eb7f2dd68fd11bd2ce745dbd43b21b99a8
qualicontaxanderson-hub <qualicontax.anderson@gmail.com>
2025-11-27 20:48:34 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,5 +5,6 @@ db = SQLAlchemy()
 from models.usuario import Usuario
 from models.rota import Rota
 from models.motorista import Motorista
+from models.fornecedor import Fornecedor  # nova model
 
-__all__ = ['Usuario', 'Rota', 'Motorista', 'db']
+__all__ = ['Usuario', 'Rota', 'Motorista', 'Fornecedor', 'db']
